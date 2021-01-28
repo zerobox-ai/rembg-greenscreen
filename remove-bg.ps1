e309173053698d904382cfbacc9906cdee8b4fab
Tim Scarfe <tim@developer-x.com>
2021-01-28 19:47:23 +0000

    tweaks

--- remove-bg.ps1
+++ remove-bg.ps1
@@ -1,73 +1,67 @@
-param([int]$batches,
-[int]$concurrency,
-[string]$target,
-[int]$fps,
-[bool]$skip_create_frames)
+param(
+    [string]$target,    
+[int]$batches=10,
+[int]$concurrency=4,
+[int]$fps=30,
+[bool]$skip_create_frames=0,
+[bool]$fix_in_place=0)
 
-mkdir ./tmp -Force
-cp $target ./tmp
-cd tmp
+if( !$fix_in_place){
+    
+    $tempfolder = $target -replace ".mp4", ""
 
-$folder = Get-Location
+    mkdir ./$tempfolder -Force
+    cp $target ./$tempfolder
+    cd $tempfolder
 
-# turn video into frames
-if(-not $skip_create_frames){
-    ffmpeg -i $target -vf fps=$fps %d.jpg
+    $folder = Get-Location
+
+    # turn video into frames
+    if(-not $skip_create_frames){
+        ffmpeg -i $target -vf fps=$fps %d.jpg
+    }
+
 }
 
-# delete any failed png conversions
-ls *.png | where Length -eq 0 | del
-
-$d = 0;
-
-# process all the frames into batches
-Get-ChildItem | 
-    Where-Object Name -imatch "jpg" |
-    ForEach-Object { 
-        New-Object PSObject  -property @{
-            Test= -not (Test-Path -Path ($_.name -replace "jpg", ".out.png") ); 
-            Path=$_.Name;
-            Number=$d++
-        } } | 
-        Where-Object Test | 
-        Group-Object -Property {$_.Number % $batches} |
-        ForEach-Object{
-            $gn = $_.Name
-            mkdir $gn -Force
-
-            $_.Group | ForEach-Object{ Copy-Item $_.Path $gn }
-
-            Start-ThreadJob -ThrottleLimit $concurrency -ScriptBlock {param([string]$folder, [string]$gn)
-
-                C:/Windows/System32/cmd.exe /K "$env:anaconda\\Scripts\\activate.bat $env:anaconda && cd $env:rembg && python -m src.rembg.cmd.cli -p $folder\$gn 2>&1 && exit"
-            
-                Copy-Item $folder/$gn/*.png $folder
-                Remove-Item $folder/$gn/ -Force -Recurse
-
-            } -ArgumentList "$folder", "$gn"
-
-        }
-
-get-job | Wait-Job
-
-
 # retry failed ones
-1..2 | % { 
+1..3 | % { 
 
-    # do a second pass to pick up the failed ones
-    $failed = ls *.png | where Length -eq 0 |  %{ $_.Name -replace ".out.png", "" }
+    # delete any failed png conversions
+    ls *.png | where Length -eq 0 | del
 
-    mkdir failed -Force
-    $failed | ForEach-Object{ Copy-Item "$_.jpg" failed }
+    $d = 0;
 
-    if( ($failed | Measure-Object ).count -gt 0 )
-    {
-        C:/Windows/System32/cmd.exe /K "$env:anaconda\\Scripts\\activate.bat $env:anaconda && cd $env:rembg && python -m src.rembg.cmd.cli -p $folder\failed 2>&1 && exit"
+    # process all the frames into batches
+    Get-ChildItem | 
+        Where-Object Name -imatch "jpg" |
+        ForEach-Object { 
+            New-Object PSObject  -property @{
+                Test= -not (Test-Path -Path ($_.name -replace "jpg", ".out.png") ); 
+                Path=$_.Name;
+                Number=$d++
+            } } | 
+            Where-Object Test | 
+            Group-Object -Property {$_.Number % $batches} |
+            ForEach-Object{
+                $gn = $_.Name
+                mkdir $gn -Force
 
-        Copy-Item $folder/failed/*.png $folder
-        Remove-Item $folder/failed/ -Force -Recurse
-    }
- }
+                $_.Group | ForEach-Object{ Copy-Item $_.Path $gn }
+
+                Start-ThreadJob -ThrottleLimit $concurrency -ScriptBlock {param([string]$folder, [string]$gn)
+
+                    C:/Windows/System32/cmd.exe /K "$env:anaconda\\Scripts\\activate.bat $env:anaconda && cd $env:rembg && python -m src.rembg.cmd.cli -p $folder\$gn 2>&1 && exit"
+                
+                    Copy-Item $folder/$gn/*.png $folder
+                    Remove-Item $folder/$gn/ -Force -Recurse
+
+                } -ArgumentList "$folder", "$gn"
+
+            }
+
+    get-job | Wait-Job
+
+}
 
 #check for every JPG file there is a corresponding non-empty PNG present
 if( (Get-ChildItem | 
@@ -84,8 +78,17 @@ if( (Get-ChildItem |
     
             ffmpeg -i %d.out.png -vcodec png ($target -replace ".mp4", ".mov")
         
+            Move-Item *.mov ../
+
             del *.png
+
+            cd ../
+            del $tempfolder -Recurse
+}
+
+else{
+    cd ../
 }
 
 # go back
-cd ../
+
