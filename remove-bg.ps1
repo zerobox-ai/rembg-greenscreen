f2d450606bfa776c42f4bb43841a073a53ec029f
Tim Scarfe <tim@developer-x.com>
2021-01-29 14:42:07 +0000

    Robustness++
    * No longer conflate batches adn concurrency
    * Now can continue after a break, will keep converted pngs from batches, and delete old batches

--- remove-bg.ps1
+++ remove-bg.ps1
@@ -1,12 +1,12 @@
 param(
-    [string]$target,    
-[int]$batches=10,
-[int]$concurrency=4,
+[string]$target,    
+[int]$batches=6,
 [int]$fps=30,
 [int]$retry_times=2)
 
-
 $tempfolder = $target -replace ".mp4", ""
+
+echo $tempfolder
 
 mkdir ./$tempfolder -Force
 cp $target ./$tempfolder
@@ -27,10 +27,30 @@ if(-not $skip_create_frames){
 # retry failed ones
 1..$retry_times | % { 
 
+    echo "moving pngs from old batches up"
+
+    # copy down any previously completed pngs from other
+    # runs which have been prematurely terminated
+    ls -Directory | %{ 
+        $pngs = ls ("./{0}/*.png" -f $_.Name)
+        if( $pngs.Count -gt 0 ){  
+            mv -Force ($pngs) ./  
+        }
+    }
+
+    echo "deleting old batches"
+
+    # delete any previous batch folders
+    del (ls -Directory) -Force -Recurse
+
+    echo "deleting failed conversions"
+
     # delete any failed png conversions
     ls *.png | where Length -eq 0 | del
 
     $d = 0;
+
+    echo "creating new batches and executing"
 
     # process all the frames into batches
     Get-ChildItem | 
@@ -49,7 +69,7 @@ if(-not $skip_create_frames){
 
                 $_.Group | ForEach-Object{ Copy-Item $_.Path $gn }
 
-                Start-ThreadJob -ThrottleLimit $concurrency -ScriptBlock {param([string]$folder, [string]$gn)
+                Start-Job -ScriptBlock {param([string]$folder, [string]$gn)
 
                     C:/Windows/System32/cmd.exe /K "$env:anaconda\\Scripts\\activate.bat $env:anaconda && cd $env:rembg && python -m src.rembg.cmd.cli -p $folder\$gn 2>&1 && exit"
                 
@@ -75,21 +95,17 @@ if( $equal_pngs -and $noempty_pngs){
 
     echo "equal jpgs and pngs detected, and all pngs are non-zero, making mov file..."
 
-    del *.jpg
-
     ffmpeg -i %d.out.png -vcodec png ($target -replace ".mp4", ".mov")
 
     echo "moving mov file back to main dir"
     Move-Item *.mov ../
-
+    del *.jpg
     del *.png
     cd ../
     del $tempfolder -Recurse
 }
 
 else{
+    # go back
     cd ../
 }
-
-# go back
-
