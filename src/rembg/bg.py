38af3918cab2e905aadb9af869e51790d2edef43
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-02-17 10:45:01 +0100

    perf(bg): move more compute to GPU

--- src/rembg/bg.py
+++ src/rembg/bg.py
@@ -6,7 +6,6 @@ import requests
 import torch
 import torch.nn.functional
 from hsh.library.hash import Hasher
-from skimage import transform
 from tqdm import tqdm
 
 from .u2net import u2net
@@ -14,18 +13,8 @@ from .u2net import u2net
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
+@torch.no_grad()
 def remove_many(image_data: typing.List[np.array], model_name: str):
-    original_shape = image_data[0].shape[:2]
-    resized = [transform.resize(image, (320, 320)) for image in image_data]  # expensive
-    # note that transform.resize will return values on [0,1] )(float64)
-    # so it silently converts them from 255 uint8
-    np_arrays = [np.array(image) for image in resized]
-    master_images = np.array(np_arrays).astype(np.float32)
-
-    master_images = (master_images - 0.485) / 0.229
-
-    # move color chanel to second
-    master_images = np.moveaxis(master_images, 3, 1)
     hasher = Hasher()
 
     model, hash_val, drive_target, env_var = {
@@ -78,11 +67,16 @@ def remove_many(image_data: typing.List[np.array], model_name: str):
     net.load_state_dict(torch.load(path))
     net.to(DEVICE)
     net.eval()
-    with torch.no_grad():
-        out = net(torch.as_tensor(master_images, device=DEVICE, dtype=torch.float32))[0][:, 0, :, :]
-        ma = torch.max(out)
-        mi = torch.min(out)
-        dn = (out - mi) / (ma - mi) * 255
-        dn = torch.nn.functional.interpolate(dn, original_shape, mode='linear')
-        dn = dn.to(dtype=torch.uint8, device=torch.device('cpu')).detach().numpy()
+    image_data = np.stack(image_data)
+    original_shape = image_data.shape[1:]
+    image_data = torch.as_tensor(image_data, dtype=torch.float32, device=DEVICE)
+    image_data = torch.nn.functional.interpolate(image_data, (320, 320), mode='linear')
+    image_data = (image_data / 255 - 0.485) / 0.229
+    image_data = torch.transpose(image_data, 1, 3)
+    out = net(image_data)[:, 0, :, :]
+    ma = torch.max(out)
+    mi = torch.min(out)
+    dn = (out - mi) / (ma - mi) * 255
+    dn = torch.nn.functional.interpolate(dn, original_shape, mode='linear')
+    dn = dn.to(dtype=torch.uint8, device=torch.device('cpu')).detach().numpy()
     return dn
