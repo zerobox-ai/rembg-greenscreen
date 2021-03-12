15e182f7fdca0fc74b385f8246b85692feb62f62
Tim Scarfe <tim@developer-x.com>
2021-03-12 16:20:18 +0000

    fixes

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="rembg-greenscreen",
-    version="2.1.6",
+    version="2.1.9",
     description="Rembg Virtual Green Screen",
     long_description=long_description,
     long_description_content_type="text/markdown",
